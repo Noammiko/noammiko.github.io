8d6ec1c47c502712370b866e4b48ea6be4749a55
zivoy <ziv132@gmail.com>
2024-11-07 22:57:46 -0500

    add seo thing

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,7 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://noam.shalit.name',
-    integrations: [
-        react(),
-        tailwind({
-            applyBaseStyles: false,
-        }),
-        svelte()
-    ],
+    integrations: [react(), tailwind({
+        applyBaseStyles: false,
+    }), svelte()],
 });
