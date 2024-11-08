4e64629459054f126ec1a0d363feba5ec4e9a7c6
zivoy <ziv132@gmail.com>
2024-11-07 23:30:45 -0500

    switch to bun, fixes bug with pnpm and commonjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,6 @@ export default defineConfig({
     }), svelte(), opengraphImages({
         options: {
         },
-        render: presets.blackAndWhite
+        render: presets.simpleBlog
     })],
 });
