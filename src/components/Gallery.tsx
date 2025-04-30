aeac9856a4a7fc7b6fa536f3165de68d4bd65581
zivoy <ziv132@gmail.com>
2025-04-30 19:40:48 -0400

    make images loop

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -3,43 +3,46 @@ import Autoplay from "embla-carousel-autoplay"
 
 
 import {
-	Carousel,
-	CarouselContent,
-	CarouselItem,
-	CarouselNext,
-	CarouselPrevious,
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
 } from "@/components/ui/carousel"
 import type { GetImageResult } from "astro";
 
 interface CarouselItemProps {
-	images: Array<GetImageResult>;
-	className?: string;
-	buttons?: boolean;
+  images: Array<GetImageResult>;
+  className?: string;
+  buttons?: boolean;
 }
 
 export default function CarouselComponent({ images, className, buttons = true }: CarouselItemProps) {
-	if (images === undefined || images.length === 0) return (<></>);
+  if (images === undefined || images.length === 0) return (<></>);
 
-	return (
-		<Carousel className={`w-full max-w-xs ${className}`}
-			plugins={[
-				Autoplay({
-					delay: 2000,
-				}),
-			]}>
-			<CarouselContent>
-				{images.map((image, index) => (
-					<CarouselItem key={index}>
-						<div className="p-1">
-							<OptimizedImage OptimizedImage={image} className="rounded-lg" />
-						</div>
-					</CarouselItem>
-				))}
-			</CarouselContent>
-			{buttons && <>
-				<CarouselPrevious />
-				<CarouselNext />
-			</>}
-		</Carousel>
-	)
+  return (
+    <Carousel className={`w-full max-w-xs ${className}`}
+      opts={{
+        loop: true,
+      }}
+      plugins={[
+        Autoplay({
+          delay: 2000,
+        }),
+      ]}>
+      <CarouselContent>
+        {images.map((image, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <OptimizedImage OptimizedImage={image} className="rounded-lg" />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      {buttons && <>
+        <CarouselPrevious />
+        <CarouselNext />
+      </>}
+    </Carousel>
+  )
 }
