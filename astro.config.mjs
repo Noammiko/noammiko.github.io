a3b523eee117a9582475f4fbeacc06a4a9f36182
zivoy <ziv132@gmail.com>
2025-05-03 10:17:15 -0400

    fix up upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
-
-import tailwind from "@astrojs/tailwind";
-
 import opengraphImages, { presets } from "astro-opengraph-images";
 
 import icon from "astro-icon";
@@ -11,37 +7,45 @@ import icon from "astro-icon";
 import plguins from "./lucide.config";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 const { createLucideAstroImportOptimizer: lucideAstroImportOptimizer, lucideSvelteImportOptimizer } = plguins;
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://miko-recordingstudio.ca",
-  integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  }), opengraphImages({
-    options: {
-      fonts: [
-        {
-          name: "Roboto",
-          weight: 400,
-          style: "normal",
-          data: fs.readFileSync(
-            "node_modules/@fontsource/roboto/files/roboto-latin-400-normal.woff",
-          ),
-        },
-      ],
-    },
-    render: presets.blackAndWhite,
-  }), icon(), sitemap(), svelte()],
+  integrations: [
+    opengraphImages({
+      options: {
+        fonts: [
+          {
+            name: "Roboto",
+            weight: 400,
+            style: "normal",
+            data: fs.readFileSync(
+              "node_modules/@fontsource/roboto/files/roboto-latin-400-normal.woff",
+            ),
+          },
+        ],
+      },
+      render: presets.blackAndWhite,
+    }),
+    icon(),
+    sitemap(),
+    svelte(),
+    react(),
+  ],
   vite: {
     plugins: [
       lucideAstroImportOptimizer(),
       // lucideSvelteImportOptimizer(),
+      tailwindcss({
+        applyBaseStyles: false,
+      }),
     ],
     resolve: {
       conditions: ["browser"],
-      alias: {
-      },
+      alias: {},
     },
   },
 });
