80f26f67e931802ed0e5659084bfb9fe94a755ec
zivoy <ziv132@gmail.com>
2025-05-05 12:33:54 -0400

    start of music component

---
+++ src/components/musicPortfolio/audioComponent.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { playing } from "./utils.js";
+
+  const song = $derived(
+    $playing === null
+      ? {
+          file: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+          title: "test",
+        }
+      : $playing,
+  );
+
+  import AudioPlayer from "./audioPlayer.svelte";
+</script>
+
+<AudioPlayer
+  title={song.title}
+  src={song.file}
+  close={() => ($playing = null)}
+/>
