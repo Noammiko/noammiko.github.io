35208068d7bf1a043b4f8846e5c03960771e080c
zivoy <ziv132@gmail.com>
2025-05-22 14:19:57 -0400

    open in new tab

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -1,17 +1,21 @@
-import React from "react";
+import * as React from "react";
 import { Button, type ButtonProps } from "./button";
 
-export interface LinkProps extends ButtonProps {
-  href: string;
+// Exclude keys from anchor props that are already in ButtonProps
+type AnchorProps = Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof ButtonProps>;
+
+export interface LinkProps extends ButtonProps, AnchorProps {
   children?: React.ReactNode;
 }
 
-const Link: React.FC<LinkProps> = ({ href, children, ...props }) => {
+const Link: React.FC<LinkProps> = ({ href, target, children, ...props }) => {
   return (
     <Button {...props} asChild>
-      <a href={href}>{children}</a>
+      <a href={href} target={target}>{children}</a>
     </Button>
   );
 };
+
+Link.displayName = "Link";
 
 export { Link };
