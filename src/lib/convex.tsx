d2afd800c989f43721f41afc8567913d5478cf0d
zivoy <ziv132@gmail.com>
2025-05-22 11:06:46 -0400

    add auth

--- src/lib/convex.tsx
+++ src/lib/convex.tsx
@@ -1,4 +1,5 @@
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import type { ComponentType } from "react";
 
 import { CONVEX_URL } from "astro:env/client";
@@ -6,14 +7,19 @@ import { CONVEX_URL } from "astro:env/client";
 // Initialized once so all components share the same client.
 const client = new ConvexReactClient(CONVEX_URL);
 
+type ProviderType = "regular" | "auth";
+
 export function withConvexProvider<P extends object>(
-	Component: ComponentType<P>
+	Component: ComponentType<P>,
+	providerType: ProviderType = "regular"
 ) {
 	return function WithConvexProvider(props: P) {
+		const Provider = providerType === "auth" ? ConvexAuthProvider : ConvexProvider;
+
 		return (
-			<ConvexProvider client={client}>
+			<Provider client={client}>
 				<Component {...props} />
-			</ConvexProvider>
+			</Provider>
 		);
 	};
 }
