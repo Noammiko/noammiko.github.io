cbc595609dddef22d16c08f2370bb72550f132e7
zivoy <ziv132@gmail.com>
2025-05-22 14:26:39 -0400

    open link for fill out

--- src/components/priceNbundles/freeTrail.tsx
+++ src/components/priceNbundles/freeTrail.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Link } from "@/components/ui/link";
 import { withConvexProvider } from "@/lib/convex";
 import { useMemo } from "react";
 
@@ -79,12 +79,14 @@ const FreeTrial: React.FC<FreeTrialProps> = ({
         </li>
       </ul>
 
-      <Button
+      <Link
         className="w-full bg-yellow-500 text-black font-bold py-3 hover:bg-yellow-600 transition-colors"
         disabled={progress === 1}
+        target="_blank"
+        href="https://docs.google.com/forms/d/e/1FAIpQLScnNSlUUQYPwaDyjP9A595vw-R-twFaiCpmdMGiHJLfF1UtcQ/viewform?usp=dialog"
       >
         Secure Your Spot
-      </Button>
+      </Link>
     </div>
   );
 };
